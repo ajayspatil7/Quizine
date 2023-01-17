f5e9bf8b775e11aa739c1a7229c48eceeb43bab4
ajayspatil7 <aspatil99451@gmail.com>
2023-01-17 11:35:52 +0530

    Some push and commit.

---
+++ dump/dapAssig.py
@@ -0,0 +1,2 @@
+# WAP in C that accepts twp decimal numbers, have two pointers to these numbers and write two function, add and subtract use call by reference print in main() function the sum and difference only if it is exceding the value 10.
+#
--- root/main.py
+++ root/main.py
@@ -1,10 +1,4 @@
 
 from root import examInterface
-
-#  Password is 0
-#  Import login module and use it to login
-#  If the password is correct, open the exam interface
-
-
 app = examInterface.ExamApp()
 app.mainloop()
